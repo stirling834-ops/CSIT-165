5c0a7545c2a620c614690a1f7d314f6526bba337
RWPalomar <rwilliams1739@student.palomar.edu>
2026-04-02 13:19:46 -0700

    Completed Objective 4 in Project 1, and the changes have been saved to the tester and the RMarkdown file

--- Project-1/Project 1 Tester.R
+++ Project-1/Project 1 Tester.R
@@ -64,4 +64,30 @@ distance_miles <- distance_meters/1609.344
 print(paste(latest_region, "is", distance_miles, "miles away from", origin_confirmations))
 
 # Objective 4
-# 
+# Getting information to create a new dataframe for risk score assessment
+# This includes States/Provinces, Countries/Regions, Latitude, Longitude, Confirmations, Deaths, and the Risk Score
+# This will be important due to needing to filter out all instances of cases and deaths on cruise ships
+# It also makes it nicer for seeing the data all in one place
+filtered_states <- confirmations[confirmations$Lat != 0 & !is.na(confirmations$Lat),1]
+filtered_regions <- confirmations[confirmations$Lat != 0 & !is.na(confirmations$Lat),2]
+filtered_lat <- confirmations[confirmations$Lat != 0 & !is.na(confirmations$Lat),3]
+filtered_long <- confirmations[confirmations$Lat != 0 & !is.na(confirmations$Lat),4]
+filtered_confirmations <- confirmations[confirmations$Lat != 0 & !is.na(confirmations$Lat),length(confirmations[1,])]
+filtered_deaths <- deaths[deaths$Lat != 0 & !is.na(deaths$Lat),length(deaths[1,])]
+# Calculating the risk scores for every region
+risk_scores <- 100*(filtered_deaths/filtered_confirmations)
+# Creating a vector for the headers of each column in the new dataframe
+headers <- c("Province/State", "Country/Region", "Lat", "Long", "Confirmations", "Deaths", "Risk Score")
+# Creating the dataframe
+risk_assessment <- data.frame(filtered_states, filtered_regions, filtered_lat, filtered_long, filtered_confirmations, filtered_deaths, risk_scores)
+colnames(risk_assessment) <- headers
+# Evaluating which areas have the highest and lowest risk scores
+# First the row number needs to be retrieved
+max_risk_row = which.max(risk_assessment[, 7])
+min_risk_row = which.min(risk_assessment[, 7])
+# Next, the region names associated with the max and min risk are extracted
+max_risk_region = risk_assessment[max_risk_row, 2]
+min_risk_region = risk_assessment[min_risk_row, 2]
+# Printing the results
+print(paste("The region with the highest risk is", max_risk_region))
+print(paste("The region with the lowest risk is", min_risk_region))
